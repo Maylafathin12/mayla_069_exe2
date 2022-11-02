4d2f3cd8fb70666163487976f2c24f9ff9506825
TA1069 <maylafaat@gmail.com>
2022-11-02 15:51:42 +0700

    menambahkan perulangan while

---
+++ Mayla Fathin_20210140069_Exe2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Mayla_Fathin_20210140069_Exe2
+{
+   class Exe2
+    {
+        //mendeklarasikan array int dengan ukuran 89
+        //yang diambil dari 2 digit terakhir nim = 69
+        public decimal[] mayla = new decimal[89];
+
+        //jumlah array yang akan tersimpan
+        public decimal n;
+
+        //membuat fungsi method
+        public void Input()
+        {
+            while (true)
+            {
+                //digunakan untuk menampilkan  masukkan banyaknya elemen pada array
+                Console.WriteLine("Masukkan banyaknya elemen pada array : ");
+            }
+        }
+    }
+}
